b827045a78869de96ca06c2a3ed382ef53f1ac50
James Morris <jamesm@torchbox.com>
2013-03-13 15:30:33 +0000

    JS test

--- js/main.js
+++ js/main.js
@@ -33,7 +33,8 @@ $(function() {
     */
     var axel = Math.random() + "";
     var a = axel * 10000000000000;
-    document.write('<iframe src="https://3395873.fls.doubleclick.net/activityi;src=3395873;type=rnibo778;cat=rnibc051;ord=' + a + '?" width="1" height="1" frameborder="0" style="display:none"></iframe>');
+    
+    //document.write('<iframe src="https://3395873.fls.doubleclick.net/activityi;src=3395873;type=rnibo778;cat=rnibc051;ord=' + a + '?" width="1" height="1" frameborder="0" style="display:none"></iframe>');
   }  
 
   //Newsletter thank you message control
